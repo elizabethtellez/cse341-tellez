1bec93a44bae17259a36da3e39c076d7d9b34d7c
elizabethtellez <tel21004@byui.edu>
2023-01-10 20:31:17 -0800

    port change

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3000
 
 app.use('/', require('./routes'));
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is running on port ${port}');
 });
